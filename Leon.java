1ee9e5d13bd56b89bfcaa11a730fdfb1768d8ebd
lSylra <143262189+lSylra@users.noreply.github.com>
2023-10-16 19:56:04 -0500

    Add files via upload

---
+++ Leon.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebaanimal;
+
+/**
+ *
+ * @author USER
+ */
+public class Leon extends Felino {
+    public String getSonido() {
+        return "Rugido";
+    }
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
+    public String getHabitat() {
+        return "Praderas";
+    }
+    public String getNombreCientifico() {
+        return "Panthera leo";
+    }
+}
---
+++ Lobo.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pruebaanimal;
+
+/**
+ *
+ * @author USER
+ */
+public class Lobo extends Canido{
+    public String getSonido() {
+        return "Aullido";
+    }
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
+    public String getHabitat() {
+        return "Bosque";
+    }
+    public String getNombreCientifico() {
+        return "Canis lupus";
+    }
+}
